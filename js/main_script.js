0bcbd8a6768802ac4c9852638abf6314f88f7489
felipeweil <felipeweilmar@gmail.com>
2025-04-26 13:02:03 +0200

    Removed img & description from #entries
    - Also changed click event to not deppend on the img
    - Cleaned up html visually

--- js/main_script.js
+++ js/main_script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   $(document).ready(function () {
+
     $.get("https://botw-compendium.herokuapp.com/api/v3/compendium/all", function (data) {
       const allEntries = data.data;
       const randomEntries = [];
@@ -24,50 +25,59 @@ $(document).ready(function () {
 
       // Display the "correctEntry" in the regular entries section as well
       const correctCard = `
-        <div class="col-md-4">
+        <div class="col-md-4 entry correct-entry">
           <div class="card h-100 shadow-sm">
-            <img src="${correctEntry.image}" class="card-img-top" alt="${correctEntry.name}" data-entry="correct">
             <div class="card-body">
               <h5 class="card-title text-capitalize">${correctEntry.name}</h5>
-              <p class="card-text"><strong>Category:</strong> ${correctEntry.category}</p>
-              ${correctEntry.description ? `<p class="card-text">${correctEntry.description}</p>` : ''}
             </div>
           </div>
         </div>
-      `;
+        `;
+        // <img src="${correctEntry.image}" class="card-img-top" alt="${correctEntry.name}" data-entry="correct">
+        // <p class="card-text"><strong>Category:</strong> ${correctEntry.category}</p>
+        // ${correctEntry.description ? `<p class="card-text">${correctEntry.description}</p>` : ''}
+      
       $('#entries').append(correctCard);
-
+          
       // Display the other entries in the regular section
       randomEntries.forEach((entry, index) => {
         if (index !== correctEntryIndex) {
           const card = `
-            <div class="col-md-4">
+            <div class="col-md-4 entry">
               <div class="card h-100 shadow-sm">
-                <img src="${entry.image}" class="card-img-top" alt="${entry.name}" data-entry="incorrect">
                 <div class="card-body">
                   <h5 class="card-title text-capitalize">${entry.name}</h5>
-                  <p class="card-text"><strong>Category:</strong> ${entry.category}</p>
-                  ${entry.description ? `<p class="card-text">${entry.description}</p>` : ''}
                 </div>
               </div>
             </div>
-          `;
+            `;
+                
           $('#entries').append(card);
         }
+
+        // <img src="${entry.image}" class="card-img-top" alt="${entry.name}" data-entry="incorrect">
+        // <p class="card-text"><strong>Category:</strong> ${entry.category}</p>
+        // ${entry.description ? `<p class="card-text">${entry.description}</p>` : ''}
       });
 
+
+
       // Add click event to entry images
-      $('#entries img').click(function () {
-        const clickedImage = $(this).attr('src');
+      $('.entry').on("click", function() {
+        let $this = $(this);
+
+        console.log($this + " has been clicked");
         
-        if (clickedImage === correctImageUrl) {
+
+        if ($this.hasClass("correct-entry")) {
           // Show the correct message
           $('#resultMessage').show();
+          console.log("Correct entry has been clicked");
         } else {
-          // Hide the correct message if the answer is wrong
-          $('#resultMessage').hide();
+          console.log("INcorrect entry has been clicked"); 
         }
-      });
+
+    });
     });
   });
 
